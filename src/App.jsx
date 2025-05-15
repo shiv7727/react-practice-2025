d5edbfaec4a769f20569c0cb9eabd873e41ceff0
shiv gaurav <shivgaurav27@gmail.com>
2025-05-15 11:59:19 +0530

    useReducer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useReducer } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'INC':
+			return { count: state.count + action.payload };
+		case 'DEC':
+			return { count: state.count - action.payload };
+		default:
+			return state;
+	}
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+	const [state, dispatch] = useReducer(reducer, { count: 0 });
+	return (
+		<div>
+			<h3>UseReducer example</h3>
+			<h4>Count:{state.count}</h4>
+			<button onClick={() => dispatch({ type: 'INC', payload: 5 })}>INC</button>
+			<button onClick={() => dispatch({ type: 'DEC', payload: 5 })}>DEC</button>
+		</div>
+	);
+};
 
-export default App
+export default App;
