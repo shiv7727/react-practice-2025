2694a62a156996851a9993386da06c72f14dd8bc
shiv gaurav <shivgaurav27@gmail.com>
2025-05-15 08:32:01 +0530

    stopwatch working full

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect, useRef } from 'react';
+import './App.css';
+const App = () => {
+	const id = useRef(null);
+	const [time, setTime] = useState({
+		h: 0,
+		m: 0,
+		s: 0,
+	});
 
-function App() {
-  const [count, setCount] = useState(0)
+	const handleTime = () => {
+		id.current = setInterval(() => {
+			setTime((time) => {
+				if (time.s === 60) {
+					return { ...time, s: 0, m: time.m + 1 };
+				}
+				if (time.m === 60) {
+					return { ...time, s: 0, m: 0, h: time.h + 1 };
+				}
+				return { ...time, s: time.s + 1 };
+			});
+		}, 1000);
+	};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+	useEffect(() => {
+		return () => clearInterval(id.current);
+	}, []);
 
-export default App
+	return (
+		<div>
+			<h2>stopWatch</h2>
+			<h3>
+				{time.s}:{time.m}:{time.h}
+			</h3>
+			<hr />
+			<button onClick={handleTime}>Start</button>
+			<button
+				onClick={() => {
+					clearInterval(id.current);
+				}}
+			>
+				Pause
+			</button>
+			<button
+				onClick={() => {
+					clearInterval(0);
+					setTime({
+						h: 0,
+						m: 0,
+						s: 0,
+					});
+				}}
+			>
+				Reset
+			</button>
+		</div>
+	);
+};
+
+export default App;
